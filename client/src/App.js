28101090276e08c5f5a93e978f210a3c1dc0e0c7
That_Guy <eghove@gmail.com>
2019-02-23 12:27:13 -0600

    template for app.js

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// pull in the React router
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+
+// import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        
+      </div>
+    </Router>
+  )
+}
+
+export default App;
