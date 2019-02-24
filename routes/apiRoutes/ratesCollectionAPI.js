2a9eb4c5be2d9fb1a389545fee400df3cd418842
That_Guy <eghove@gmail.com>
2019-02-24 11:15:13 -0600

    scaffolding the ratesCollectionAPI route

---
+++ routes/apiRoutes/ratesCollectionAPI.js
@@ -0,0 +1,10 @@
+// THE API ROUTE TO GET THE NECESSARY RATES FOR ESTIMATION
+
+const router = require("express").Router();
+// pull in the rates controller
+
+// matches with "/api/estimate"
+router.route("/rates")
+
+// export the router
+module.exports = router;
