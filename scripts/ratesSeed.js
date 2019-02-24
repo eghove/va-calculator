a66182a5686d005b114d53a38fd59f9f12bcdaed
That_Guy <eghove@gmail.com>
2019-02-23 18:42:16 -0600

    got the surviving spouse data added

--- scripts/ratesSeed.js
+++ scripts/ratesSeed.js
@@ -12,7 +12,7 @@ mongoose.connect(
 
 const ratesSeed = [
 
-  // === VETERANS RATES ====
+  // === VETERAN RATES ====
   {
     // 12/01/2018 RATES
     effectiveDate: "2018-12-01",
@@ -114,6 +114,112 @@ const ratesSeed = [
     hbRateAddOne: 19710,
     // aid and attendance rate with one dependent
     aaRateAddOne: 25448,
+    // the additional allowance for dependents after the add one rate
+    moreDepsRate: 2198
+  },
+
+  // === SURVIVNG SPOUSE RATES ====
+  {
+    // 12/01/2018 RATES
+    effectiveDate: "2018-12-01",
+    // The claimantType (i.e. Veteran, Surviving Spouse, Surviving Child)
+    claimantType: "Surviving Spouse",
+    // the base rate without any dependents
+    baseRate: 9078,
+    // housebound rate with no dependents
+    hbRate: 11095,
+    // aid and attendance rate with no dependents
+    aaRate: 14509,
+    // the base rate with one dependent
+    baseRateAddOne: 11881,
+    // housebound rate with one dependent
+    hbRateAddOne: 13893,
+    // aid and attendance rate with one dependent
+    aaRateAddOne: 17309,
+    // the additional allowance for dependents after the add one rate
+    moreDepsRate: 2313
+  },
+
+  {
+    // 12/01/2017 RATES
+    effectiveDate: "2017-12-01",
+    // The claimantType (i.e. Veteran, Surviving Spouse, Surviving Child)
+    claimantType: "Surviving Spouse",
+    // the base rate without any dependents
+    baseRate: 8830,
+    // housebound rate with no dependents
+    hbRate: 10792,
+    // aid and attendance rate with no dependents
+    aaRate: 14113,
+    // the base rate with one dependent
+    baseRateAddOne: 11557,
+    // housebound rate with one dependent
+    hbRateAddOne: 13514,
+    // aid and attendance rate with one dependent
+    aaRateAddOne: 16837,
+    // the additional allowance for dependents after the add one rate
+    moreDepsRate: 2250
+  },
+
+  {
+    // 12/01/2016 RATES
+    effectiveDate: "2016-12-01",
+    // The claimantType (i.e. Veteran, Surviving Spouse, Surviving Child)
+    claimantType: "Surviving Spouse",
+    // the base rate without any dependents
+    baseRate: 8656,
+    // housebound rate with no dependents
+    hbRate: 10580,
+    // aid and attendance rate with no dependents
+    aaRate: 13836,
+    // the base rate with one dependent
+    baseRateAddOne: 11330,
+    // housebound rate with one dependent
+    hbRateAddOne: 13249,
+    // aid and attendance rate with one dependent
+    aaRateAddOne: 16506,
+    // the additional allowance for dependents after the add one rate
+    moreDepsRate: 2205
+  },
+
+  {
+    // 12/01/2015 RATES (Same as 12/01/2014, no COLA)
+    effectiveDate: "2015-12-01",
+    // The claimantType (i.e. Veteran, Surviving Spouse, Surviving Child)
+    claimantType: "Surviving Spouse",
+    // the base rate without any dependents
+    baseRate: 8630,
+    // housebound rate with no dependents
+    hbRate: 10548,
+    // aid and attendance rate with no dependents
+    aaRate: 13794,
+    // the base rate with one dependent
+    baseRateAddOne: 11296,
+    // housebound rate with one dependent
+    hbRateAddOne: 13209,
+    // aid and attendance rate with one dependent
+    aaRateAddOne: 16456,
+    // the additional allowance for dependents after the add one rate
+    moreDepsRate: 2198
+  },
+
+  {
+    // 12/01/2014 RATES 
+    effectiveDate: "2014-12-01",
+    // The claimantType (i.e. Veteran, Surviving Spouse, Surviving Child)
+    claimantType: "Surviving Spouse",
+    // the base rate without any dependents
+    baseRate: 8630,
+    // housebound rate with no dependents
+    hbRate: 10548,
+    // aid and attendance rate with no dependents
+    aaRate: 13794,
+    // the base rate with one dependent
+    baseRateAddOne: 11296,
+    // housebound rate with one dependent
+    hbRateAddOne: 13209,
+    // aid and attendance rate with one dependent
+    aaRateAddOne: 16456,
     // the additional allowance for dependents after the add one rate
     moreDepsRate: 2198
   },
