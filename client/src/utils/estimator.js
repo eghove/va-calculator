0b42c826535d7dabba5c0ba0f19f6b2a081e138e
That_Guy <eghove@gmail.com>
2019-02-25 11:33:03 -0600

    method for calculating the medical expense deduction

--- client/src/utils/estimator.js
+++ client/src/utils/estimator.js
@@ -1,1 +1,14 @@
 // the logic for the estimator
+
+// for testing
+var a = process.argv[2];
+
+const estimator = {
+  // method that takes in the baseRate, calculates the medical expense deduction.
+  calculateMedDed: function (baseRate) {
+    // the medical expense deduction is 5% of the baserate, rounded down to the nearest dollar.
+    let medicalDeduction = parseInt(baseRate) * .05;
+    // rounds it down to the nearest whole dollar and returns the value.
+    return medicalDeduction = parseInt(medicalDeduction);
+  }
+}
