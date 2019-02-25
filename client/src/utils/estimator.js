fe6de5c2306b1d04b111f29e6589a359907dc543
That_Guy <eghove@gmail.com>
2019-02-25 15:04:07 -0600

    method to annualize a single medical expense built

--- client/src/utils/estimator.js
+++ client/src/utils/estimator.js
@@ -18,7 +18,17 @@ const estimator = {
     let annualAmt = parseFloat(monthlyAmount) * 12;
     // returns it rounded down to the nearest whole dollar
     return annualAmt = parseInt(annualAmt);
+  },
+
+  // method that takes in a single monthly medical expense and annualizes it.
+  calcAnnSingExp: function (monthlyAmount) {
+    // multiplies the monthly expense by 12 to annualize it. NOTE: Medical expenses DO NOT get rounded down to the nearest dollar at this stage.
+    let annualAmt = parseFloat(monthlyAmount) * 12;
+    // round down to two decimal places and return the value
+    return annualAmt = Math.round(annualAmt * 100) / 100;
   }
 }
 
+// THE TESTING FUNCTION
 // console.log(estimator.calcAnnSingInc(a));
+console.log(estimator.calcAnnSingExp(a));
