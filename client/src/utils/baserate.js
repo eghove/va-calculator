93abcaafe654255937c5db9481ed1bd08fb9a9a7
That_Guy <eghove@gmail.com>
2019-02-25 20:59:41 -0600

    starting

---
+++ client/src/utils/baserate.js
@@ -0,0 +1,10 @@
+
+const baseRate = {
+  // take in the number of dependents; housebound or aa election (or none); and the rates object
+  calculate: function(claimantType, dependents, hbaa, rates) {
+    if (claimantType !== "Surviving Child") {
+      if 
+    }
+    
+
+}
