22ab6d4ebcda376d38188b4ec13cef1eebf146ad
KOB <kobbootcamp@gmail.com>
2019-02-28 16:23:27 -0600

    added nav.css and app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
-// pull in the React router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import QuestionnairePage from "./pages/QuestionnairePage"
+import NoMatch from "./pages/NoMatch"
+import Nav from "./components/Nav"
 
 
 // import './App.css';
@@ -9,7 +11,12 @@ function App() {
   return (
     <Router>
       <div>
-        
+        <Nav />
+        <Switch>
+            {/* <Route exact path="/" component={} /> */}
+            <Route exact path="/questions" component={QuestionnairePage} />
+            <Route component={NoMatch} />
+        </Switch>
       </div>
     </Router>
   )
