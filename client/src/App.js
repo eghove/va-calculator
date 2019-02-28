7427e855f2390d5acac121cc3f48d292805f1354
Spencer Caretta <spencer.caretta@gmail.com>
2019-02-28 17:29:57 -0600

    moved files from login folder to main folder and changed up some routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,35 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import QuestionnairePage from "./pages/QuestionnairePage"
 import NoMatch from "./pages/NoMatch"
 import Nav from "./components/Nav"
-
+import Login from './Login';
+import Home from './Home/Home';
+import Callback from './Callback/Callback';
+import Auth from './Auth/Auth';
+import history from './history';
 
 // import './App.css';
 
+const auth = new Auth();
+
+const handleAuthentication = ({location}) => {
+  if (/access_token|id_token|error/.test(location.hash)) {
+    auth.handleAuthentication();
+  }
+}
+
 function App() {
   return (
-    <Router>
+    <Router history={history}>
       <div>
         <Nav />
         <Switch>
             {/* <Route exact path="/" component={} /> */}
+            <Route path="/" render={(props) => <Login auth={auth} {...props} />} />
+            <Route path="/home" render={(props) => <Home auth={auth} {...props} />} />
+            <Route path="/callback" render={(props) => {
+              handleAuthentication(props);
+              return <Callback {...props} /> 
+            }}/>
             <Route exact path="/questions" component={QuestionnairePage} />
             <Route component={NoMatch} />
         </Switch>
