bb5d6a72c6687121ece7b301f8efb8df4239a796
KOB <kobbootcamp@gmail.com>
2019-03-01 16:23:58 -0600

    working routes!

--- client/src/pages/ResultsPage.js
+++ client/src/pages/ResultsPage.js
@@ -4,17 +4,19 @@ import './QuestionnairePage.css';
 //import ReactDOM from 'react-dom';
 //import Button from '@material-ui/core/Button';
 
-class Results extends Component {
-  render() {
+  function Results () {
     return (
+      <div>
       <Grid container spacing = {24}>
         <Grid item xs={12} id='surveyTitle' ><h4>Results</h4></Grid>
       </Grid>
       <Grid container spacing = {24}>
         <Grid item xs={12} id='surveyTitle' ><h5>You may be eligible for...</h5></Grid>
       </Grid>
+      </div>
+
     );
   }
-}
+
 
 export default Results;
