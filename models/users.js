68f86632e97e93276abb23e6dc3aa575889abacc
That_Guy <eghove@gmail.com>
2019-03-04 19:37:12 -0600

    started building the users schema

---
+++ models/users.js
@@ -0,0 +1,11 @@
+// the users model
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const usersSchema = new Schema({
+  userName: {type: String, required: true}
+});
+
+const Users = mongoose.model("Users", usersSchema);
+
+module.exports = Users;
