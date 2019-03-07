24e1301567855f99c7e1e84e66d817584a16fcad
Spencer Caretta <spencer.caretta@gmail.com>
2019-03-06 20:41:52 -0600

    auth0 variable change for heroku

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
-    domain: 'dev-lk5g0n3x.auth0.com',
-    clientId: '2uUsy8IVMwRpaketcJOz75qIXTSgVaGh',
-    callbackUrl: 'http://localhost:3000/callback'
+    domain: process.env.AUTH0_DOMAIN || 'dev-lk5g0n3x.auth0.com',
+    clientId: process.env.AUTH0_CLIENT_ID || '2uUsy8IVMwRpaketcJOz75qIXTSgVaGh',
+    callbackUrl: process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
   }
