4c31d0a8972f7929a0f63414a265163a22502d3a
That_Guy <eghove@gmail.com>
2019-03-10 16:57:47 -0500

    got the backend of the get route built

--- routes/apiRoutes/userAPI.js
+++ routes/apiRoutes/userAPI.js
@@ -3,9 +3,17 @@
 const router = require("express").Router();
 // pull in the rates controller
 const userController = require("../../controllers/userController");
+const estimatorController = require("../../controllers/estimatorController");
 
-// matches with "api/user"
+// matches with "api/users"
 router.route("/create")
   .post(userController.createUser);
+
+
+
+// matches with "api/users/get-estimate"
+router.route("/get-estimate/:id")
+  .get(estimatorController.findEstimates);
+
 // export the router
 module.exports = router;
