fdac60d37c80c8cf00a354999705f6d50eb51ba0
That_Guy <eghove@gmail.com>
2019-03-13 19:03:34 -0500

    took some unused variables out of questionnairepage.js

--- client/src/pages/QuestionnairePage.js
+++ client/src/pages/QuestionnairePage.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import SimpleSelect from "../components/Survey";
 import Grid from '@material-ui/core/Grid';
 import './QuestionnairePage.css';
-import Footer from '../components/Footer';
+
 
 
 function QuestionnairePage () {
